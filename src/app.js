85e50c88142bc401ad2da6be0eb5911ef8600bd2
Shoriful <shorifulbba0360@gmail.com>
2025-04-14 08:21:49 +0600

    added userInfo save in database by base of role

--- src/app.js
+++ src/app.js
@@ -11,23 +11,71 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+// // rabbani vai code 
+// app.post('/user/:email', async (req, res) => {
+//    const user = req.body;
+//    const email = req.params.email;
+//    const query = { email };
+
+//    // Check if user already exists
+//    const isExist = await userCollection.findOne(query);
+//    if (isExist) {
+//        return res.send(isExist); // If user exists, return existing user
+//    }
+
+//    // Prepare the user data based on role
+//    const newUser = {
+//        name: user.name,
+//        email: user.email,
+//        role: user.role,
+//        photoURL: user.photoURL || "",
+//    };
+
+//    // If Employer, include company information
+//    if (user.role === "Employer") {
+//        newUser.companyName = user.companyName || "";
+//        newUser.companyDetails = user.companyDetails || "";
+//    }
+
+//    try {
+//        // Insert new user into collection
+//        const result = await userCollection.insertOne(newUser);
+//        res.send(result);
+//    } catch (error) {
+//        console.error("Error inserting user:", error);
+//        res.status(500).send({ error: true, message: "Server error while creating user" });
+//    }
+// });
 //create User
 app.post('/register', async (req, res) => {
-   const {email,password} = req.body;
-   const existingUser=await userCollection.findOne({email});
+   const userData = req.body;
+   const existingUser=await userCollection.findOne({email:userData?.email});
    if(existingUser){
       return res.status(400).json({message:"Email already exists"})
    }
-   const hashedPassword=await bcrypt.hash(password,10)
-   const result=await userCollection.insertOne({
-      email,
-      password:hashedPassword,
-      loginAttempts: 0,          // Initial login attempts counter
-      lockUntil: null,
-   })
-   res.send(result)
+
+
+   if(userData.password){
+      const hashedPassword=await bcrypt.hash(userData.password,10)
+      const result=await userCollection.insertOne({
+         ...userData,
+           password:hashedPassword,
+           loginAttempts: 0,          // Initial login attempts counter
+           lockUntil: null,
+        })
+       return  res.send(result)
+   }else{
+      const result=await userCollection.insertOne({
+         ...userData,
+        })
+        res.send(result)
+   }
+  
  
 })
+
+
 
 // block user when enter wrong password
 
